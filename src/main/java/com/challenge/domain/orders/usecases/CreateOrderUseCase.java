02376e763f4593ac5b86ae616c030ac94f0cbe43
emersonbnp <emersonbnp@gmail.com>
2025-03-13 11:27:53 -0300

    Adds logger and moves file to correct package

--- src/main/java/com/challenge/domain/orders/usecases/CreateOrderUseCase.java
+++ src/main/java/com/challenge/domain/orders/usecases/CreateOrderUseCase.java
@@ -1,14 +1,18 @@
-package com.challenge.domain.orders.domain.orders.usecases;
+package com.challenge.domain.orders.usecases;
 
 import com.challenge.domain.exceptions.DuplicateOrderException;
-import com.challenge.domain.orders.domain.orders.models.Order;
-import com.challenge.domain.orders.domain.orders.repository.OrderRepository;
+import com.challenge.domain.orders.models.Order;
+import com.challenge.domain.orders.repository.OrderRepository;
 import com.challenge.domain.services.CacheService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class CreateOrderUseCase {
 
     private final CacheService cacheService;
     private final OrderRepository orderRepository;
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     public CreateOrderUseCase(CacheService cacheService, OrderRepository orderRepository) {
         this.cacheService = cacheService;
@@ -16,7 +20,9 @@ public class CreateOrderUseCase {
     }
 
     public String execute(Order order) {
+        logger.debug("Creating order: {}", order);
         if (cacheService.exists(order.orderDeduplicationCode())) {
+            logger.warn("Order already exists.");
             throw new DuplicateOrderException(String.format("Duplicate order for customer %s", order.getCustomerUuid()));
         }
 
@@ -26,6 +32,8 @@ public class CreateOrderUseCase {
                 .reduce(Double::sum);
         order.setTotalPrice(totalPrice.orElse(0.0));
 
-        return orderRepository.saveOrder(order);
+        var orderUuid = orderRepository.saveOrder(order);
+        logger.info("Created order uuid: {}", orderUuid);
+        return orderUuid;
     }
 }
